5e0d3689146923eb71afe6007cf11276b91284d6
Jenson-Bolton <jensonbolton@outlook.com>
2021-11-02 18:24:36 +0000

    Started everything again

--- script.js
+++ script.js
@@ -1,36 +1,5 @@
 // initialise canvas
-var c = document.querySelector('canvas');
+c = document.querySelector('canvas');
 c.width = window.innerWidth;
 c.height = window.innerHeight;
-var ctx = c.getContext('2d');
-
-class Object2D {
-    constructor(width, height, posX, posY, color) {
-        this.width = width;
-        this.height = height;
-        this.posX = posX;
-        this.posY = posY;
-        this.color = color;
-    }
-
-    clear() {
-        ctx.clearRect()
-    }
-
-    draw(x, y) {
-        ctx.fillStyle(this.color);
-        ctx.fillRect(x, y, )
-    }
-
-    moveXY(x, y) {
-
-    }
-
-    moveUp(step) {
-
-    }
-}
-
-class Paddle extends Object2D {
-
-}
+ctx = c.getContext('2d');
